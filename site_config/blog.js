78d180815f4091595752b5191e22a5b951b1b042
jimin.jm <jimin.jm@alibaba-inc.com>
2019-03-07 19:17:06 +0800

    add html and json

--- site_config/blog.js
+++ site_config/blog.js
@@ -1,10 +1,28 @@
 export default {
-  'en-us': {
-    barText: 'Blog',
-    postsTitle: 'All posts',
-  },
-  'zh-cn': {
-    barText: '博客',
-    postsTitle: '所有文章',
-  },
+    'en-us': {
+        barText: 'Blog',
+        postsTitle: 'All posts',
+        list: [
+            {
+                title: 'How to use Fescar to ensure consistency between Dubbo Microservices',
+                author: '@slievrly',
+                dateStr: 'Jan 17th, 2019',
+                desc: 'This blog describes details of using Fescar to ensure consistency between Dubbo Microservices',
+                link: '/en-us/blog/dubbo-fescar.html',
+            }
+        ]
+    },
+    'zh-cn': {
+        barText: '博客',
+        postsTitle: '所有文章',
+        list: [
+            {
+                title: '如何使用Fescar保证Dubbo微服务间的一致性',
+                author: '@slievrly',
+                dateStr: 'Jan 17th, 2019',
+                desc: '本文详细介绍了如何使用Fescar保证Dubbo微服务间的一致性',
+                link: '/zh-cn/blog/dubbo-fescar.html',
+            }
+        ]
+    },
 };
