4ddf592ce218f026b3de8fba7c9abd816cb14685
Xin Wang <xin.victorwang@gmail.com>
2019-05-31 14:34:51 +0800

    replace fescar with seata

--- site_config/community.jsx
+++ site_config/community.jsx
@@ -65,25 +65,13 @@ export default {
           img: '/img/mailinglist.png',
           imgHover: '/img/mailinglist_hover.png',
           title: 'Mailing List',
-          link: 'mailto:dev-subscribe@dubbo.apache.org'
-        },
-        {
-          img: '/img/alibaba.png',
-          imgHover: '/img/alibaba_hover.png',
-          title: '#alibaba/dubbo',
-          link: 'https://gitter.im/alibaba/dubbo',
-        },
-        {
-          img: '/img/segmentfault.png',
-          imgHover: '/img/segmentfault_hover.png',
-          title: 'Segment Fault',
-          link: 'https://segmentfault.com/t/dubbo'
+          link: 'mailto:dev-seata+subscribe@googlegroups.com'
         },
         {
           img: '/img/weibo.png',
           imgHover: '/img/weibo_hover.png',
-          title: '@ApacheDubbo',
-          link: 'https://twitter.com/ApacheDubbo',
+          title: '@Seata',
+          link: 'https://twitter.com/seataio',
         },
       ],
     },
@@ -99,17 +87,17 @@ export default {
         {
           img: '/img/channels/github_gray.svg',
           title: 'Issue',
-          content: <span>Submit a <a href="https://github.com/dragonflyoss/Seata/issues/new">new issue</a>.</span>,
+          content: <span>Submit a <a href="https://github.com/seata/seata/issues/new">new issue</a>.</span>,
         },
         {
           img: '/img/documents.png',
           title: 'Documents',
-          content: <span>Improve the <a href="https://github.com/dragonflyoss/website/tree/master/docs">documentation</a>.</span>,
+          content: <span>Improve the <a href="https://github.com/seata/seata.github.io">documentation</a>.</span>,
         },
         {
           img: '/img/pullrequest.png',
           title: 'Pull Request',
-          content: <span>Create a brilliant <a href="https://github.com/dragonflyoss/Seata/pulls">pull request</a>.  </span>,
+          content: <span>Create a brilliant <a href="https://github.com/seata/seata/pulls">pull request</a>.  </span>,
         },
       ],
     },
@@ -126,48 +114,7 @@ export default {
           dateStr: 'May 12nd，2018',
           link: '/zh-cn/blog/blog1.html',
         },
-        {
-          img: '/img/brhtqqzh.jpeg',
-          title: '这是标题',
-          content: '这是内容',
-          dateStr: 'May 12nd，2018',
-          link: '/zh-cn/blog/blog2.html',
-        },
-        {
-          img: '/img/brhtqqzh.jpeg',
-          title: '这是标题',
-          content: '这是内容',
-          dateStr: 'May 12nd，2018',
-          link: '/zh-cn/blog/blog3.html',
-        },
-        {
-          img: '/img/brhtqqzh.jpeg',
-          title: '这是标题',
-          content: '这是内容',
-          dateStr: 'May 12nd，2018',
-          link: '/zh-cn/blog/blog4.html',
-        },
-        {
-          img: '/img/brhtqqzh.jpeg',
-          title: '这是标题',
-          content: '这是内容',
-          dateStr: 'May 12nd，2018',
-          link: '/zh-cn/blog/blog5.html',
-        },
-        {
-          img: '/img/brhtqqzh.jpeg',
-          title: '这是标题',
-          content: '这是内容',
-          dateStr: 'May 12nd，2018',
-          link: '/zh-cn/blog/blog6.html',
-        },
-        {
-          img: '/img/brhtqqzh.jpeg',
-          title: '这是标题',
-          content: '这是内容',
-          dateStr: 'May 12nd，2018',
-          link: '/zh-cn/blog/blog7.html',
-        },
+
       ]
     },
     contacts: {
@@ -178,25 +125,13 @@ export default {
           img: '/img/mailinglist.png',
           imgHover: '/img/mailinglist_hover.png',
           title: '邮件列表',
-          link: 'mailto:dev-subscribe@dubbo.apache.org'
-        },
-        {
-          img: '/img/alibaba.png',
-          imgHover: '/img/alibaba_hover.png',
-          title: 'Gitter',
-          link: 'https://gitter.im/alibaba/dubbo',
-        },
-        {
-          img: '/img/segmentfault.png',
-          imgHover: '/img/segmentfault_hover.png',
-          title: 'Segment Fault',
-          link: 'https://segmentfault.com/t/dubbo'
+          link: 'mailto:dev-seata+subscribe@googlegroups.com'
         },
         {
           img: '/img/weibo.png',
           imgHover: '/img/weibo_hover.png',
-          title: '@ApacheDubbo',
-          link: 'https://twitter.com/ApacheDubbo',
+          title: '@Seata',
+          link: 'https://twitter.com/seataio',
         },
       ],
     },
@@ -212,17 +147,17 @@ export default {
         {
           img: '/img/channels/github_gray.svg',
           title: '报告问题',
-          content: <span>提交<a href="https://github.com/dragonflyoss/Seata/issues/new">新问题</a>。</span>,
+          content: <span>提交<a href="https://github.com/seata/seata/issues/new">新问题</a>。</span>,
         },
         {
           img: '/img/documents.png',
           title: '改进文档',
-          content: <span>改进<a href="https://github.com/dragonflyoss/website/tree/master/docs">文档</a>。</span>,
+          content: <span>改进<a href="https://github.com/seata/seata.github.io">文档</a>。</span>,
         },
         {
           img: '/img/pullrequest.png',
           title: '提交 PR',
-          content: <span>创建一个<a href="https://github.com/dragonflyoss/Seata/pulls">PR</a>。</span>,
+          content: <span>创建一个<a href="https://github.com/seata/seata/pulls">PR</a>。</span>,
         },
       ],
     },
