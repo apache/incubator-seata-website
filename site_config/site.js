dd4dd01f7a6685ef6dc62eea49c3022a35888606
withthewind <withthewind_github@aliyun.com>
2019-11-18 14:58:36 +0800

    (zh-cn) Add download pageMenu And Add download.md

--- site_config/site.js
+++ site_config/site.js
@@ -90,6 +90,11 @@ export default {
         text: '社区',
         link: '/zh-cn/community/index.html',
       },
+      {
+        key: 'download',
+        text: '下载',
+        link: '/zh-cn/blog/download.html',
+      },
     ],
     vision: {
       title: '愿景',
