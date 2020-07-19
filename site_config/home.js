663ea3986ad3b2a85bdcf9fa252b45bc017ace44
FUNKYE <364176773@qq.com>
2020-07-19 16:21:45 +0800

    update home.js

--- site_config/home.js
+++ site_config/home.js
@@ -56,7 +56,7 @@ export default {
                 },
                 {
                     icon: 'feature-5',
-                    title: 'XA 模式（开发中）',
+                    title: 'XA 模式',
                     content: '支持已实现 XA 接口的数据库的 XA 模式',
                 },
                 {
