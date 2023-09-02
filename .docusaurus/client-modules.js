79b1a1b5b0df5bf092eb6d21074ea6b77c442c06
chai001125 <1637519169@qq.com>
2023-09-03 06:39:42 +0800

    删除无用文件

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/chaixinyi/Documents/new/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/chaixinyi/Documents/new/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/chaixinyi/Documents/new/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/chaixinyi/Documents/new/src/css/custom.scss'),
-];
