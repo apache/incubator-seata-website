813eb5e9b0ec422bd36c292817cf88777c12e664
liuqiufeng <775038282@qq.com>
2023-09-07 11:16:52 +0800

    auto obtain the latest version info of github

--- src/pages/home/top/index.tsx
+++ src/pages/home/top/index.tsx
@@ -21,7 +21,10 @@ const topData = {
       type: 'normal',
     },
     {
-      text: translate({ id: 'homepage.userregistrationButton', message: '用户登记' }),
+      text: translate({
+        id: 'homepage.userregistrationButton',
+        message: '用户登记',
+      }),
       link: 'https://github.com/seata/seata/issues/1246',
       type: 'normal',
       target: '_blank',
@@ -45,30 +48,35 @@ const topData = {
       target: '_blank',
     },
   ],
-  versionNote: {
-    text: 'Release Note of 1.7.0',
-    link: 'https://github.com/seata/seata/releases/tag/v1.7.0',
-  },
-  releaseDate: {
-    text: 'Released on Jul 11, 2023',
-  },
 };
 
 const Top = ({ language }: { language?: string }) => {
-  const [state, setState] = React.useState({
+  const [repo, setRepo] = React.useState({
     starCount: '',
     forkCount: '',
   });
-
-  const { starCount, forkCount } = state;
+  const [releaseNote, setReleaseNote] = React.useState({
+    version: '',
+    url: '',
+    date: '',
+  });
 
   React.useEffect(() => {
     fetch('//api.github.com/repos/seata/seata')
       .then((res) => res.json())
       .then((data) => {
-        setState({
+        setRepo({
           starCount: `${data.stargazers_count}`,
           forkCount: `${data.forks_count}`,
+        });
+      });
+    fetch('https://api.github.com/repos/seata/seata/releases/latest')
+      .then((res) => res.json())
+      .then((data) => {
+        setReleaseNote({
+          version: data.name,
+          url: data.html_url,
+          date: new Date(data.published_at).toLocaleDateString(),
         });
       });
   }, []);
@@ -76,71 +84,111 @@ const Top = ({ language }: { language?: string }) => {
   return (
     <BrowserOnly>
       {() => (
-        <section className="top-section">
-          <div className="top-body">
-            <div className="vertical-middle">
-              <div className="product-name">
+        <section className='top-section'>
+          <div className='top-body'>
+            <div className='vertical-middle'>
+              <div className='product-name'>
                 <h2>{topData.brandName}</h2>
               </div>
-              <p className="product-desc">{topData.briefIntroduction}</p>
-              <div className="button-area">
+              <p className='product-desc'>{topData.briefIntroduction}</p>
+              <div className='button-area'>
                 {topData.buttons.map((b) => (
-                  <Button type={b.type as ButtonType} key={b.text} link={b.link} target={b.target} language={language}>
+                  <Button
+                    type={b.type as ButtonType}
+                    key={b.text}
+                    link={b.link}
+                    target={b.target}
+                    language={language}
+                  >
                     {b.text}
                   </Button>
                 ))}
               </div>
-              <div className="github-buttons">
-                <a href="https://github.com/seata/seata" target="_blank" rel="noopener noreferrer">
-                  <div className="star">
-                    <img src="https://img.alicdn.com/tfs/TB1FlB1JwHqK1RjSZFPXXcwapXa-32-32.png" />
-                    <span className="type">Star</span>
-                    <span className="line" />
-                    <span className="count">{starCount}</span>
+              <div className='github-buttons'>
+                <a
+                  href='https://github.com/seata/seata'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <div className='star'>
+                    <img src='https://img.alicdn.com/tfs/TB1FlB1JwHqK1RjSZFPXXcwapXa-32-32.png' />
+                    <span className='type'>Star</span>
+                    <span className='line' />
+                    <span className='count'>{repo.starCount}</span>
                   </div>
                 </a>
-                <a href="https://github.com/seata/seata/fork" target="_blank" rel="noopener noreferrer">
-                  <div className="fork">
-                    <img src="https://img.alicdn.com/tfs/TB1zbxSJwDqK1RjSZSyXXaxEVXa-32-32.png" />
-                    <span className="type">Fork</span>
-                    <span className="line" />
-                    <span className="count">{forkCount}</span>
+                <a
+                  href='https://github.com/seata/seata/fork'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <div className='fork'>
+                    <img src='https://img.alicdn.com/tfs/TB1zbxSJwDqK1RjSZSyXXaxEVXa-32-32.png' />
+                    <span className='type'>Fork</span>
+                    <span className='line' />
+                    <span className='count'>{repo.forkCount}</span>
                   </div>
                 </a>
               </div>
-              <div className="version-note">
-                <a target="_blank" rel="noopener noreferrer" href={getLink(topData.versionNote.link)}>
-                  {topData.versionNote.text}
+              <div className='version-note'>
+                <a
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href={releaseNote.url}
+                >
+                  Release Note of {releaseNote.version}
                 </a>
               </div>
-              <div className="release-date">{topData.releaseDate.text}</div>
+              <div className='release-date'>Released on {releaseNote.date}</div>
             </div>
-            <div className="animation">
-              <img className="img1" src="//img.alicdn.com/tfs/TB1evnpJhnaK1RjSZFBXXcW7VXa-702-312.png" />
-              <img className="img2" src="//img.alicdn.com/tfs/TB1iau9JcbpK1RjSZFyXXX_qFXa-914-1156.png" />
-              <div className="outer-circle" />
-              <div className="rotate-circle">
-                <svg viewBox="0 0 404 404" xmlns="http://www.w3.org/2000/svg">
+            <div className='animation'>
+              <img
+                className='img1'
+                src='//img.alicdn.com/tfs/TB1evnpJhnaK1RjSZFBXXcW7VXa-702-312.png'
+              />
+              <img
+                className='img2'
+                src='//img.alicdn.com/tfs/TB1iau9JcbpK1RjSZFyXXX_qFXa-914-1156.png'
+              />
+              <div className='outer-circle' />
+              <div className='rotate-circle'>
+                <svg viewBox='0 0 404 404' xmlns='http://www.w3.org/2000/svg'>
                   <defs>
                     <linearGradient
-                      id="linear"
-                      x1="0%"
-                      y1="0%"
-                      x2="100%"
-                      y2="0%"
+                      id='linear'
+                      x1='0%'
+                      y1='0%'
+                      x2='100%'
+                      y2='0%'
                     >
-                      <stop offset="0%" stopColor="rgba(17, 186, 250, 1)" />
-                      <stop offset="50%" stopColor="rgba(17, 186, 250, 0.1)" />
-                      <stop offset="50%" stopColor="rgba(17, 186, 250, 1)" />
-                      <stop offset="100%" stopColor="rgba(17, 186, 250, 0.1)" />
+                      <stop offset='0%' stopColor='rgba(17, 186, 250, 1)' />
+                      <stop offset='50%' stopColor='rgba(17, 186, 250, 0.1)' />
+                      <stop offset='50%' stopColor='rgba(17, 186, 250, 1)' />
+                      <stop offset='100%' stopColor='rgba(17, 186, 250, 0.1)' />
                     </linearGradient>
                   </defs>
-                  <circle cx="202" cy="202" r="200" fill="rgba(0, 0, 0, 0)" stroke="url(#linear)" strokeWidth="4" />
+                  <circle
+                    cx='202'
+                    cy='202'
+                    r='200'
+                    fill='rgba(0, 0, 0, 0)'
+                    stroke='url(#linear)'
+                    strokeWidth='4'
+                  />
                 </svg>
               </div>
-              <img className="img3" src="//img.alicdn.com/tfs/TB1EBu.JgHqK1RjSZJnXXbNLpXa-914-1156.png" />
-              <img className="img4" src="//img.alicdn.com/tfs/TB115i2JmzqK1RjSZPxXXc4tVXa-186-78.png" />
-              <img className="img5" src="//img.alicdn.com/tfs/TB115i2JmzqK1RjSZPxXXc4tVXa-186-78.png" />
+              <img
+                className='img3'
+                src='//img.alicdn.com/tfs/TB1EBu.JgHqK1RjSZJnXXbNLpXa-914-1156.png'
+              />
+              <img
+                className='img4'
+                src='//img.alicdn.com/tfs/TB115i2JmzqK1RjSZPxXXc4tVXa-186-78.png'
+              />
+              <img
+                className='img5'
+                src='//img.alicdn.com/tfs/TB115i2JmzqK1RjSZPxXXc4tVXa-186-78.png'
+              />
             </div>
           </div>
         </section>
