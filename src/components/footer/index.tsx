43c4113af95532e556d9f05be0a3eaea02d9a011
liuqiufeng <775038282@qq.com>
2023-12-07 20:41:33 +0800

    incubating

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -9,7 +9,7 @@ const data = {
     content: translate({
       id: 'homepage.footerVersionContent',
       message:
-        'Apache Seata 是一款开源的分布式事务解决方案，致力于在微服务架构下提供高性能和简单易用的分布式事务服务。',
+        'Apache Seata (incubating) 是一款开源的分布式事务解决方案，致力于在微服务架构下提供高性能和简单易用的分布式事务服务。',
     }),
   },
   documentation: {
