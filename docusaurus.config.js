bf9da88ba43528e9767ba25f01a676a8cab5faf0
liuqiufeng <775038282@qq.com>
2024-01-09 16:31:40 +0800

    优化项目名称

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const asfDict = [
 ];
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Apache Seata (incubating)',
+  title: 'Apache Seata™',
   favicon: 'img/seata_logo_small.jpeg',
 
   // Set the production url of your site here
@@ -242,7 +242,7 @@ const config = {
         copyright: `
                   <div class="fs-12">
                     <div class="center-div">
-                      <span>Apache Seata is an effort undergoing incubation at The Apache Software Foundation (ASF), sponsored by the Apache Incubator. Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF.</span>
+                      <span>Apache Seata (incubating) is an effort undergoing incubation at The Apache Software Foundation (ASF), sponsored by the Apache Incubator. Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF.</span>
                     </div>
                     <br/>
                     <div class="center-div">
