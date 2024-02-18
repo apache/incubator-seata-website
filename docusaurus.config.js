863b516621ba0cbe14419a045b15a0705d0f44de
liuqiufeng <775038282@qq.com>
2024-02-18 09:52:28 +0800

    处理兼容问题

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const { themes } = require('prism-react-renderer');
+const lightCodeTheme = themes.github;
+const darkCodeTheme = themes.dracula;
 const asfDict = [
   { target: 'https://www.apache.org', text: 'Foundation' },
   { target: 'https://www.apache.org/licenses', text: 'License' },
@@ -64,6 +65,36 @@ const config = {
       async: true,
     },
   ],
+  headTags: [
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'aes-config',
+        content: 'pid=xux-opensource&user_type=101&uid=&username=&dim10=seata',
+      },
+    },
+    {
+      tagName: 'script',
+      attributes: {
+        type: 'text/javascript',
+        src: 'https://hm.baidu.com/hm.js?104e73ef0c18b416b27abb23757ed8ee',
+      },
+    },
+    {
+      tagName: 'script',
+      attributes: {
+        src: '//g.alicdn.com/alilog/mlog/aplus_v2.js',
+        id: 'beacon-aplus',
+        exparams: 'clog=o&aplus&sidx=aplusSidx&ckx=aplusCkx',
+      },
+    },
+    {
+      tagName: 'script',
+      attributes: {
+        src: '//g.alicdn.com/aes/??tracker/1.0.34/index.js,tracker-plugin-pv/2.4.5/index.js,tracker-plugin-event/1.2.5/index.js,tracker-plugin-jserror/1.0.13/index.js,tracker-plugin-api/1.1.14/index.js,tracker-plugin-perf/1.1.8/index.js,tracker-plugin-eventTiming/1.0.4/index.js',
+      },
+    },
+  ],
   plugins: [
     'docusaurus-plugin-sass',
     [
@@ -80,46 +111,6 @@ const config = {
         editLocalizedFiles: true,
       }),
     ],
-    // [
-    //   'docusaurus-plugin-includes',
-    //   {
-    //     injectedHtmlTags: {
-    //       headTags: [
-    //         {
-    //           tagName: 'meta',
-    //           attributes: {
-    //             name: 'aes-config',
-    //             content:
-    //               'pid=xux-opensource&user_type=101&uid=&username=&dim10=seata',
-    //           },
-    //         },
-    //       ],
-    //       preBodyTags: [
-    //         {
-    //           tagName: 'script',
-    //           attributes: {
-    //             type: 'text/javascript',
-    //             src: 'https://hm.baidu.com/hm.js?104e73ef0c18b416b27abb23757ed8ee',
-    //           },
-    //         },
-    //         {
-    //           tagName: 'script',
-    //           attributes: {
-    //             src: '//g.alicdn.com/alilog/mlog/aplus_v2.js',
-    //             id: 'beacon-aplus',
-    //             exparams: 'clog=o&aplus&sidx=aplusSidx&ckx=aplusCkx',
-    //           },
-    //         },
-    //         {
-    //           tagName: 'script',
-    //           attributes: {
-    //             src: '//g.alicdn.com/aes/??tracker/1.0.34/index.js,tracker-plugin-pv/2.4.5/index.js,tracker-plugin-event/1.2.5/index.js,tracker-plugin-jserror/1.0.13/index.js,tracker-plugin-api/1.1.14/index.js,tracker-plugin-perf/1.1.8/index.js,tracker-plugin-eventTiming/1.0.4/index.js',
-    //           },
-    //         },
-    //       ],
-    //     },
-    //   },
-    // ],
   ],
   presets: [
     [
