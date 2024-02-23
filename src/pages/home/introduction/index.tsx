1442d0ddcc595125e5f97ae790da597214480c7a
liuqiufeng <775038282@qq.com>
2024-02-23 09:41:55 +0800

    optimize: Optimize the display of static images on the home page of zh-cn in development mode. (#845)

--- src/pages/home/introduction/index.tsx
+++ src/pages/home/introduction/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { translate } from '@docusaurus/Translate';
 import './index.scss';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 const data = {
   title: translate({ id: 'homepage.introTitle', message: 'Seata 是什么？' }),
@@ -8,9 +9,6 @@ const data = {
     id: 'homepage.introDesc',
     message:
       'Seata 是一款开源的分布式事务解决方案，致力于在微服务架构下提供高性能和简单易用的分布式事务服务。在 Seata 开源之前，其内部版本在阿里系内部一直扮演着应用架构层数据一致性的中间件角色，帮助经济体平稳的度过历年的双11，对上层业务进行了有力的技术支撑。经过多年沉淀与积累，其商业化产品先后在阿里云、金融云上售卖。2019.1 为了打造更加完善的技术生态和普惠技术成果，Seata 正式宣布对外开源，未来 Seata 将以社区共建的形式帮助用户快速落地分布式事务解决方案。',
-  }),
-  img: translate({
-    id: 'homepage.introImg',
   }),
 };
 
@@ -22,7 +20,9 @@ const Introduction = () => {
           <h3>{data.title}</h3>
           <p>{data.desc}</p>
         </div>
-        <img src={data.img} />
+        <img
+          src={useBaseUrl('/img/index/TB1rDpkJAvoK1RjSZPfXXXPKFXa-794-478.png')}
+        />
       </div>
     </section>
   );
--- src/pages/home/top/index.tsx
+++ src/pages/home/top/index.tsx
@@ -3,6 +3,7 @@ import { translate } from '@docusaurus/Translate';
 import { Button, ButtonType } from '../../../components';
 
 import './index.scss';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 const topData = {
   brandName: 'Apache Seata(incubating)',
@@ -82,7 +83,11 @@ const Top = () => {
               rel='noopener noreferrer'
             >
               <div className='star'>
-                <img src='/img/index/TB1FlB1JwHqK1RjSZFPXXcwapXa-32-32.png' />
+                <img
+                  src={useBaseUrl(
+                    '/img/index/TB1FlB1JwHqK1RjSZFPXXcwapXa-32-32.png'
+                  )}
+                />
                 <span className='type'>Star</span>
                 <span className='line' />
                 <span className='count'>{repo.starCount}</span>
@@ -94,7 +99,11 @@ const Top = () => {
               rel='noopener noreferrer'
             >
               <div className='fork'>
-                <img src='/img/index/TB1zbxSJwDqK1RjSZSyXXaxEVXa-32-32.png' />
+                <img
+                  src={useBaseUrl(
+                    '/img/index/TB1zbxSJwDqK1RjSZSyXXaxEVXa-32-32.png'
+                  )}
+                />
                 <span className='type'>Fork</span>
                 <span className='line' />
                 <span className='count'>{repo.forkCount}</span>
@@ -111,11 +120,15 @@ const Top = () => {
         <div className='animation'>
           <img
             className='img1'
-            src='/img/index/TB1evnpJhnaK1RjSZFBXXcW7VXa-702-312.png'
+            src={useBaseUrl(
+              '/img/index/TB1evnpJhnaK1RjSZFBXXcW7VXa-702-312.png'
+            )}
           />
           <img
             className='img2'
-            src='/img/index/TB1iau9JcbpK1RjSZFyXXX_qFXa-914-1156.png'
+            src={useBaseUrl(
+              '/img/index/TB1iau9JcbpK1RjSZFyXXX_qFXa-914-1156.png'
+            )}
           />
           <div className='outer-circle' />
           <div className='rotate-circle'>
@@ -140,15 +153,21 @@ const Top = () => {
           </div>
           <img
             className='img3'
-            src='/img/index/TB1EBu.JgHqK1RjSZJnXXbNLpXa-914-1156.png'
+            src={useBaseUrl(
+              '/img/index/TB1EBu.JgHqK1RjSZJnXXbNLpXa-914-1156.png'
+            )}
           />
           <img
             className='img4'
-            src='/img/index/TB115i2JmzqK1RjSZPxXXc4tVXa-186-78.png'
+            src={useBaseUrl(
+              '/img/index/TB115i2JmzqK1RjSZPxXXc4tVXa-186-78.png'
+            )}
           />
           <img
             className='img5'
-            src='/img/index/TB115i2JmzqK1RjSZPxXXc4tVXa-186-78.png'
+            src={useBaseUrl(
+              '/img/index/TB115i2JmzqK1RjSZPxXXc4tVXa-186-78.png'
+            )}
           />
         </div>
       </div>
