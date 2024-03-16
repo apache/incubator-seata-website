7517069e027b36e8f567596d65281f8ee03b2eff
liuqiufeng <775038282@qq.com>
2024-03-16 22:03:17 +0800

    optimize: add apache matomo analytics

--- src/analytics.ts
+++ src/analytics.ts
@@ -4,6 +4,26 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
   if (ExecutionEnvironment.canUseDOM) {
     let s= document.getElementsByTagName('script')[0];
 
+    let matomo = document.createElement('script');
+    matomo.text = `
+      <!-- Matomo -->
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(["setDoNotTrack", true]);
+        _paq.push(["disableCookies"]);
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="https://analytics.apache.org/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '46']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+      <!-- End Matomo Code -->
+    `;
+    s.parentNode.insertBefore(matomo, s);
+
     let gtag = document.createElement('script');
     gtag.src = '/js/gtag.js';
     gtag.async = true;
