aef4e89c03ea8ae903ade8ce1f2cfb3a89b099fe
liuqiufeng <775038282@qq.com>
2024-03-20 11:45:24 +0800

    optimize: configurable analytics

--- src/analytics.ts
+++ src/analytics.ts
@@ -3,10 +3,10 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 (function () {
   if (ExecutionEnvironment.canUseDOM) {
     let prefix = '';
-    if (location.pathname.startsWith('/zh-cn')){
+    if (location.pathname.startsWith('/zh-cn')) {
       prefix = '/zh-cn';
     }
-    let s= document.getElementsByTagName('script')[0];
+    let s = document.getElementsByTagName('script')[0];
 
     let matomo = document.createElement('script');
     matomo.text = `
@@ -28,26 +28,40 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
     `;
     s.parentNode.insertBefore(matomo, s);
 
-    let gtag = document.createElement('script');
-    gtag.src = prefix + '/js/gtag.js';
-    gtag.async = true;
-    s.parentNode.insertBefore(gtag, s);
+    const controller = new AbortController();
+    const signal = controller.signal;
+    // set timeout
+    setTimeout(() => {
+      controller.abort();
+    }, 5000);
+    fetch(prefix + '/config.json', {signal})
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.analytics) {
+          let gtag = document.createElement('script');
+          gtag.src = prefix + '/js/gtag.js';
+          gtag.async = true;
+          s.parentNode.insertBefore(gtag, s);
 
-    let hm= document.createElement('script');
-    hm.src = prefix + '/js/hm.js';
-    hm.async = true;
-    s.parentNode.insertBefore(hm, s);
+          let hm = document.createElement('script');
+          hm.src = prefix + '/js/hm.js';
+          hm.async = true;
+          s.parentNode.insertBefore(hm, s);
 
-    let aplus_v2= document.createElement('script');
-    aplus_v2.src = prefix + '/js/aplus_v2.js';
-    aplus_v2.id = 'beacon-aplus';
-    aplus_v2.setAttribute('exparams','clog=o&aplus&sidx=aplusSidx&ckx=aplusCkx');
-    aplus_v2.defer= true;
-    s.parentNode.insertBefore(aplus_v2, s);
+          let aplus_v2 = document.createElement('script');
+          aplus_v2.src = prefix + '/js/aplus_v2.js';
+          aplus_v2.id = 'beacon-aplus';
+          aplus_v2.setAttribute('exparams', 'clog=o&aplus&sidx=aplusSidx&ckx=aplusCkx');
+          aplus_v2.defer = true;
+          s.parentNode.insertBefore(aplus_v2, s);
 
-    let tracker= document.createElement('script');
-    tracker.src = prefix + '/js/tracker.js';
-    tracker.defer = true;
-    s.parentNode.insertBefore(tracker, s);
+          let tracker = document.createElement('script');
+          tracker.src = prefix + '/js/tracker.js';
+          tracker.defer = true;
+          s.parentNode.insertBefore(tracker, s);
+        }
+      }).catch((err => {
+      // do nothing
+    }));
   }
 })();
